d06332df29a291004233138cd8984765e61f2029
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-04 18:33:05 +0200

    changed api

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,17 +1,17 @@
 const apiKey = 'q7MhpRuQTjsJqf9Lpi5g1tZ5exETDsyQ';
-export const location = 'Tokyo';
 
-const API_URL = `https://api.tomorrow.io/v4/weather/realtime?location=${location}&apikey=${apiKey}`;
-
-export const fetchApi = async () => {
-  const cached = localStorage.getItem('weatherData');
+export const fetchApi = async (location) => {
+  const cacheKey = `weather-${location}`;
+  const cached = localStorage.getItem(cacheKey);
 
   if (cached) {
-    console.log('📦 Dane z localStorage');
+    console.log('📦 Dane z localStorage (${location}');
     return JSON.parse(cached);
   }
 
+  const API_URL = `https://api.tomorrow.io/v4/weather/realtime?location=${location}&apikey=${apiKey}`;
   const response = await fetch(API_URL);
+
   if (!response.ok) {
     throw new Error('Nie udało się pobrać danych pogodowych');
   }
@@ -20,7 +20,7 @@ export const fetchApi = async () => {
   const weatherValues = json.data.values;
 
   // Zapisz do localStorage
-  localStorage.setItem('weatherData', JSON.stringify(weatherValues));
+  localStorage.setItem(cacheKey, JSON.stringify(weatherValues));
   console.log('🌐 Dane z API i zapisane do localStorage');
 
   return weatherValues;
