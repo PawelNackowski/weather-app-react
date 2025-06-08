87f90cb9bd7d7c9fb414503f6c6b43e11d4edc64
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-08 17:54:21 +0200

    added header subheader and weather component to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { header, subHeder } from "./namePlate";
+import { Wrapper } from "./styled";
+import { Weather } from "./Weather/Weather";
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <Wrapper>
+      <div>
+        <h1>{header}</h1>
+      </div>
+      <div>
+        <h2>{subHeder}</h2>
+      </div>
+      <div>
+        <Weather/>
+      </div>
+      </Wrapper>
+    </>
   );
 }
 
