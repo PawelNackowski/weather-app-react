db3df858a9a82be66530674bdd4e9192cd2ff5dc
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-08 17:55:11 +0200

    created global style

---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import backgroundImage from "./background.jpg"
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+    body {
+        min-height: 100vh;
+        background: url("${backgroundImage}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        ;
+    }
+`
