fd8d5086b66bb5925d9fd61b23041fa0947503b6
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-12 21:02:56 +0200

    changed direction to row and added height and gap

--- src/styled.js
+++ src/styled.js
@@ -2,6 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: center;
     align-items: center;
+    height: 100vh;
+    gap: 20px;
 `
