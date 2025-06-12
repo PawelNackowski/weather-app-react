63845977e7f6c7ad8dd631598f72a9e63a917021
Pawel Nackowski <pawelnackowski@gmail.com>
2025-06-12 21:01:48 +0200

    added default location to weather component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import { header, subHeder } from "./namePlate";
 import { Wrapper } from "./styled";
 import { Weather } from "./Weather/Weather";
 
 function App() {
   return (
     <>
-    <Wrapper>
-      <div>
-        <h1>{header}</h1>
-      </div>
-      <div>
-        <h2>{subHeder}</h2>
-      </div>
-      <div>
-        <Weather/>
-      </div>
+      <Wrapper>
+        <Weather defaultLocation="Warszawa"/>
+        <Weather defaultLocation="Nowy York"/>
+        <Weather defaultLocation="Tokio"/>
       </Wrapper>
     </>
   );
